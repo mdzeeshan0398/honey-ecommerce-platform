52a048cc868f0eb82eced886c8be7cce28d174fa
mdzeeshan0398 <156904316+mdzeeshan0398@users.noreply.github.com>
2026-01-21 16:04:51 +0530

    Delete src directory

--- src/main/java/com/honey/app/service/AuthenticationFacade.java
+++
@@ -1,7 +0,0 @@
-package com.honey.app.service;
-
-import org.springframework.security.core.Authentication;
-
-public interface AuthenticationFacade {
-    Authentication getAuthentication();
-}
--- src/main/java/com/honey/app/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.honey.app.service;
-
-import com.honey.app.io.UserRequest;
-import com.honey.app.io.UserResponse;
-
-public interface UserService {
-    UserResponse registerUser(UserRequest request);
-    String findByUserId();
-}
